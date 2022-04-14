eb4c82ff8a406c9f5e4dff482fa724cf37ff8b21
Diego Collares <diego.collares@gmail.com>
2022-04-14 08:41:57 -0300

    Create user

--- src/app/controllers/user/UserController.js
+++ src/app/controllers/user/UserController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const UserModel = require('../../models/user/UserModel');
 
 
@@ -39,6 +40,36 @@ module.exports = class UserController {
             res.status(422).json({ message: 'O campo senha e confirmação de senha não pode ser diferente'});
             return;
         }
+
+        const userExists = await UserModel.findOne({email: email});
+
+        if(userExists){
+            res.status(422).json({ message: 'Email já está uso, utilize outro email'});
+            return;
+        }
+
+        // Criar senha 
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        // Create User
+
+        const user = new UserModel({
+            name: name,
+            email: email,
+            image: image,
+            phone: phone,
+            password: passwordHash,
+        })
+
+        try {
+            const newUser = await user.save();
+            res.status(201).json({ message: 'Usuário criado com sucesso', newUser});
+        } catch (error) {
+            res.status(500).json({ message: error });
+        }
+
+
 
         return res.json({ hello: 'Hello'})
     }
