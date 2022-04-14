75a499c794cf5a71bae1f67896c0c591b127d148
Diego Collares <diego.collares@gmail.com>
2022-04-14 08:17:25 -0300

    Validate presence fileds

--- src/app/controllers/user/UserController.js
+++ src/app/controllers/user/UserController.js
@@ -3,7 +3,43 @@ const UserModel = require('../../models/user/UserModel');
 
 module.exports = class UserController {
     static async register(req, res){
-        // const { name, email, password, image, phone,  } = req.body;
+        const { name, email, password, passwordConfirmation, image, phone,  } = req.body;
+
+        if(!name){
+            res.status(422).json({ message: 'Nome é obrigátorio'});
+            return;
+        }
+
+        if(!email){
+            res.status(422).json({ message: 'Email é obrigátorio'});
+            return;
+        }
+
+        if(!password){
+            res.status(422).json({ message: 'Senha é obrigátorio'});
+            return;
+        }
+
+        if(!passwordConfirmation){
+            res.status(422).json({ message: 'Confirmação de senha é obrigátorio'});
+            return;
+        }
+
+        if(!image){
+            res.status(422).json({ message: 'Imagem é obrigátorio'});
+            return;
+        }
+
+        if(!phone){
+            res.status(422).json({ message: 'Telefone é obrigátorio'});
+            return;
+        }
+
+        if(password !== passwordConfirmation){
+            res.status(422).json({ message: 'O campo senha e confirmação de senha não pode ser diferente'});
+            return;
+        }
+
         return res.json({ hello: 'Hello'})
     }
 }
